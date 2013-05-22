43a341cf675212cc8e917c6a34981cb75c3c97d2
Peter Wilmott <p@p8952.info>
2013-05-22 07:14:40 +0100

    Error if set name begins with @ symbol

--- lib/puppet/type/package_set.rb
+++ lib/puppet/type/package_set.rb
@@ -6,6 +6,7 @@ Puppet::Type.newtype(:package_set) do
     desc 'The name of the set without @'
     isnamevar
     validate do | value |
+      raise Puppet::Error, 'Set names should not begin with @ symbols', if value.match /^@/
     end
   end
 
