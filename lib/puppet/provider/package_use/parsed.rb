848a5df3ec22e0239fdb443bc6df0485eda83e21
Chadwick Banning <chadwickbanning@gmail.com>
2013-09-05 15:29:07 -0400

    adjust relative requires for providers

--- lib/puppet/provider/package_use/parsed.rb
+++ lib/puppet/provider/package_use/parsed.rb
@@ -1,5 +1,5 @@
 require_relative '../portagefile'
-require_relative '../util/portage'
+require_relative '../../util/portage'
 
 Puppet::Type.type(:package_use).provide(:parsed,
   :parent => Puppet::Provider::PortageFile,
