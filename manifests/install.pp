55653bc4d428fdbdae854a934fa505c9b65c1493
Alexandr Mikula <mikula@fzu.cz>
2016-06-15 09:53:20 +0200

    Add keywords variable to module managed packages

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,22 +5,25 @@ class portage::install {
   portage::package { 'sys-apps/portage':
     ensure   => $portage::portage_ensure,
     keywords => $portage::portage_keywords,
+    keywords_version  => $portage::portage_keywords_version,
     use      => $portage::portage_use,
-    target   => 'portage',
+    target            => 'sys-apps.portage',
   }
 
   portage::package { 'app-portage/eix':
     ensure   => $portage::eix_ensure,
     keywords => $portage::eix_keywords,
+    keywords_version  => $portage::eix_keywords_version,
     use      => $portage::eix_use,
-    target   => 'portage',
+    target            => 'app-portage.eix',
   }
 
   portage::package { 'app-portage/layman':
     ensure   => $portage::layman_ensure,
     keywords => $portage::layman_keywords,
+    keywords_version  => $portage::layman_keywords_version,
     use      => $portage::layman_use,
-    target   => 'portage',
+    target            => 'app-portage.layman',
     notify   => Portage::Makeconf["source ${portage::layman_make_conf}"],
   }
 
@@ -37,22 +40,25 @@ class portage::install {
   portage::package { 'app-admin/webapp-config':
     ensure   => $portage::webapp_config_ensure,
     keywords => $portage::webapp_config_keywords,
+    keywords_version  => $portage::webapp_config_keywords_version,
     use      => $portage::webapp_config_use,
-    target   => 'portage',
+    target            => 'app-admin.webapp-config',
   }
 
   portage::package { 'app-admin/eselect':
     ensure   => $portage::eselect_ensure,
     keywords => $portage::eselect_keywords,
+    keywords_version  => $portage::eselect_keywords_version,
     use      => $portage::eselect_use,
-    target   => 'portage',
+    target            => 'app-admin.eselect',
   }
 
   portage::package { 'app-portage/portage-utils':
     ensure   => $portage::portage_utils_ensure,
     keywords => $portage::portage_utils_keywords,
+    keywords_version  => $portage::portage_utils_keywords_version,
     use      => $portage::portage_utils_use,
-    target   => 'portage',
+    target            => 'app-portage.portage-utils',
   }
 
 }
